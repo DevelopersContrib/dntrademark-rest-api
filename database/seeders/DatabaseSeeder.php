7162aad13050fe193aedbc407cf20b1e1732b6a3
Stephen <dev.scatacte@gmail.com>
2023-10-09 14:08:09 +0800

    Package seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Package;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,5 +20,38 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Package::truncate();
+
+        Package::create([
+            'name' => 'Free Package',
+            'start_limit' => 0,
+            'end_limit' => 1,
+            'price' => 0.00,
+        ]);
+        Package::create([
+            'name' => 'Package A',
+            'start_limit' => 1,
+            'end_limit' => 100,
+            'price' => 9.88,
+        ]);
+        Package::create([
+            'name' => 'Package B',
+            'start_limit' => 101,
+            'end_limit' => 500,
+            'price' => 48.88,
+        ]);
+        Package::create([
+            'name' => 'Package C',
+            'start_limit' => 501,
+            'end_limit' => 2000,
+            'price' => 98.88,
+        ]);
+        Package::create([
+            'name' => 'Package D',
+            'start_limit' => 2001,
+            'end_limit' => 4000,
+            'price' => 198.88,
+        ]);
     }
 }
