45154d074cec30005a2d99b35b4cb63ccd6a6114
Kareen <kjabellar@gmail.com>
2023-10-24 14:35:35 +0800

    added email

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
+        'email',
         'package_id',
         'is_onboarding',
         'is_admin'
